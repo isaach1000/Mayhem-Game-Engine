ed2171f8c492f644e028980ce3ed8ce63fa9b63f
Joseph Aharon <josaron@dyn-152-128.wilf.wireless.yu.edu>
2013-12-18 16:11:28 -0500

    Finish minheap.js

--- js/util/minheap.js
+++ js/util/minheap.js
@@ -7,8 +7,7 @@ define([], function() {
     // Private class methods/fields //
     //////////////////////////////////
 
-    // TODO
-
+    var DEFAULT_SIZE = 10;
 
     /**
      * @module modulePath // TODO: replace modulePath
@@ -18,114 +17,146 @@ define([], function() {
         // Public class methods/fields //
         /////////////////////////////////
 
-        // TODO
-
-
         /**
-         * ClassName // TODO: replace ClassName here and below
+         * @class MinHeap
          * @constructor
          */
-        MinHeap: function(size) {
+        MinHeap: function() {
             /////////////////////////////////////
             // Private instance methods/fields //
             /////////////////////////////////////
 
-            var _this = this,
-            
-            	data = new Array(size),
-            	heapSize = 0;
-            	
-            function insert(object) {
-            	heapSize++;
-            	currentIndex = heapSize - 1;
-            	data[currentIndex] = object;
-            	bubbleUp(currentIndex);
-            };  
-            
-            function extractMin() {
-            	var min = data[0];
-            	removeMin();
-            	return min;
-            };
-            
-            function bubbleUp(nodeIndex) {
-            	if (nodeIndex === 0) {
-            		return;
-            	}   
-            	var parentIndex = getParentIndex(nodeIndex);
-            	if (data[parentIndex] > data[nodeIndex] && parentIndex >= 0) {
-            		var newNodeIndex = data[parentIndex];
-            		data[parentIndex] = data[nodeIndex];
-            		data[nodeIndex] = newNodeIndex;
-            		nodeIndex = parentIndex;
-            		bubbleUp(nodeIndex);
-            	}
-            	else {
-            		return;
-            	}
-            };
-            	
-            function removeMin() {
-            	if (heapSize === 0) {
-            		return;
-            	}
-            	data[0] = data[heapSize - 1];
-            	heapSize--;
-            	if (heapSize > 0) {
-            		bubbleDown(0);
-            	}
-            }
-			
-			function bubbleDown(nodeIndex) {
-            	var 
-            		leftChildIndex = getLeftChildIndex(nodeIndex),
-            		rightChildIndex = getRightChildIndex(nodeIndex),
-            		smallerValueIndex;
-            	// This long if else assigns the smaller child
-            	if (leftChildIndex < heapSize && rightChildIndex < heapSize) {
-            		if (data[leftChildIndex] - data[rightChildIndex] < 0) {
-            			smallerValueIndex = leftChildIndex;
-            		}
-            		else {
-            			smallerValueIndex = rightChildIndex;
-            		}
-            	}
-            	else if (leftChildIndex < heapSize) {
-            		smallerValueIndex = leftChildIndex;
-            	}
-            	else if (rightChildIndex < heapSize) {
-            		smallerValueIndex = rightChildIndex;
-            	}
-            	else {
-            		return;
-            	}
-            	if (smallerValueIndex >= 0 && data[smallerValueIndex] < data[nodeIndex]) {
-            		var temp = data[nodeIndex];
-            		data[nodeIndex] = data[smallerValueIndex];
-            		data[smallerValueIndex] = temp;
-            		nodeIndex = smallerValueIndex;
-            		bubbleDown(nodeIndex);
-            	}
+            var
+                size = DEFAULT_SIZE,
+                comparator = function(data1, data2) {
+                    return data1 - data2;
+                };
+            if (arguments.length === 2) {
+                // parameters are size then comparator
+                size = arguments[0];
+                comparator = arguments[1];
+            } else if (arguments.length === 1) {
+                comparator = arguments[0];
+            } else if (arguments.length > 2) {
+                throw new Error('Invalid parameters for MinHeap constructor');
             }
 
+            var
+                _this = this,
+            	data = new Array(size),
+            	heapSize = 0;
+
+            function getLeftChildIndex(nodeIndex) {
+                return 2 * nodeIndex + 1;
+            }
+
+            function getRightChildIndex(nodeIndex) {
+                return 2 * nodeIndex + 2;
+            }
+
+            function getParentIndex(nodeIndex) {
+                return Math.floor((nodeIndex + 1) / 2) - 1;
+            }
+
+            function bubbleUp(nodeIndex) {
+                if (nodeIndex === 0) {
+                    return;
+                }
+                var parentIndex = getParentIndex(nodeIndex);
+                if (comparator(data[parentIndex], data[nodeIndex]) > 0 &&
+                    parentIndex >= 0) {
+                    var newNodeIndex = data[parentIndex];
+                    data[parentIndex] = data[nodeIndex];
+                    data[nodeIndex] = newNodeIndex;
+                    nodeIndex = parentIndex;
+                    bubbleUp(nodeIndex);
+                }
+                else {
+                    return;
+                }
+            }
+
+            function removeMin() {
+                if (heapSize === 0) {
+                    return;
+                }
+                data[0] = data[heapSize - 1];
+                heapSize--;
+                if (heapSize > 0) {
+                    bubbleDown(0);
+                }
+            }
+
+            function bubbleDown(nodeIndex) {
+                var
+                    leftChildIndex = getLeftChildIndex(nodeIndex),
+                    rightChildIndex = getRightChildIndex(nodeIndex),
+                    smallerValueIndex;
+                // This long if else assigns the smaller child
+                if (leftChildIndex < heapSize && rightChildIndex < heapSize) {
+                    if (comparator(data[leftChildIndex],
+                            data[rightChildIndex]) < 0) {
+                        smallerValueIndex = leftChildIndex;
+                    }
+                    else {
+                        smallerValueIndex = rightChildIndex;
+                    }
+                }
+                else if (leftChildIndex < heapSize) {
+                    smallerValueIndex = leftChildIndex;
+                }
+                else if (rightChildIndex < heapSize) {
+                    smallerValueIndex = rightChildIndex;
+                }
+                else {
+                    return;
+                }
+
+                if (smallerValueIndex >= 0 &&
+                    comparator(data[smallerValueIndex], data[nodeIndex]) < 0) {
+                    var temp = data[nodeIndex];
+                    data[nodeIndex] = data[smallerValueIndex];
+                    data[smallerValueIndex] = temp;
+                    nodeIndex = smallerValueIndex;
+                    bubbleDown(nodeIndex);
+                }
+            }
 
             ////////////////////////////////////
             // Public instance methods/fields //
             ////////////////////////////////////
 
-            this.getLeftChildIndex = function(nodeIndex) {
-            	return (2 * nodeIndex) + 1;
+            this.add = function(object) {
+                heapSize++;
+                var currentIndex = heapSize - 1;
+                data[currentIndex] = object;
+                bubbleUp(currentIndex);
             };
-            
-            this.getRightChildIndex = function(nodeIndex) {
-            	return (2 * nodeIndex) + 1;
-            };
-            
-            this.getParentIndex = function(nodeIndex) {
-            	return (nodeIndex - 1) / 2;
-            };
-            
 
+            this.poll = function() {
+                var min = data[0];
+                removeMin();
+                return min;
+            };
+
+            this.peek = function() {
+                return data[0];
+            };
+
+            this.clear = function() {
+                heapSize = 0;
+                data.forEach(function(element, index) {
+                    delete data[index];
+                });
+            };
+
+            Object.defineProperties(this, {
+                length: {
+                    get: function() {
+                        return heapSize;
+                    }
+                }
+            });
         }
     };
 
---
+++ js/util/searchAlgo.js
@@ -0,0 +1,46 @@
+// @formatter:off
+define([], function() {
+    "use strict";
+    // @formatter:on
+
+    //////////////////////////////////
+    // Private class methods/fields //
+    //////////////////////////////////
+
+    // TODO
+
+
+    /**
+     * @module modulePath // TODO: replace modulePath
+     */
+    var module = {
+        /////////////////////////////////
+        // Public class methods/fields //
+        /////////////////////////////////
+
+        // TODO
+
+
+        /**
+         * ClassName SearchAlgo
+         * @constructor
+         */
+        searchAlgo: function() {
+            /////////////////////////////////////
+            // Private instance methods/fields //
+            /////////////////////////////////////
+
+            var _this = this;
+            // TODO
+
+
+            ////////////////////////////////////
+            // Public instance methods/fields //
+            ////////////////////////////////////
+
+            // TODO
+        }
+    };
+
+    return module;
+});
