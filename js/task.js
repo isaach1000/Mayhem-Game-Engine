99f447c97530c185f8b852fc5dcf785ca7b97425
Isaac Hier <isaachier@gmail.com>
2013-12-31 21:21:48 -0500

    Get rid of debug statements.
    Signed-off-by: Isaac Hier <isaachier@gmail.com>

--- js/task.js
+++ js/task.js
@@ -12,21 +12,19 @@ require.config({
 });
 
 addEventListener('message', function(mainEvent) {
-    console.debug('hello');
-});
+    require(['util/graph'], function(Graph) {
+        'use strict';
 
-require(['util/graph'], function(Graph) {
-    'use strict';
-
-    var graph = new Graph.Graph();
-    var a = graph.addNode('a');
-    var b = graph.addNode('b');
-    var c = graph.addNode('c');
-    graph.addEdge(a, b);
-    graph.addEdge(a, c);
-    graph.addEdge(b, c);
-    graph.addEdge(c, b);
-    graph.addEdge(c, a);
-    var dict = graph.toDictionary();
-    console.debug(JSON.stringify(dict));
+        var graph = new Graph.Graph();
+        var a = graph.addNode('a');
+        var b = graph.addNode('b');
+        var c = graph.addNode('c');
+        graph.addEdge(a, b);
+        graph.addEdge(a, c);
+        graph.addEdge(b, c);
+        graph.addEdge(c, b);
+        graph.addEdge(c, a);
+        var dict = graph.toDictionary();
+        console.debug(JSON.stringify(dict));
+    });
 });
