96471432415c2ece8fa3cf3582c314dbf19e4c92
Isaac Hier <isaachier@gmail.com>
2013-12-31 20:28:08 -0500

    Change the title of the project.
    Signed-off-by: Isaac Hier <isaachier@gmail.com>

--- js/task.js
+++ js/task.js
@@ -1,40 +1,32 @@
-// This function contains all of the logic for the worker
-function messageHandler(mainEvent) {
-    importScripts('./lib/require.js');
+importScripts('./lib/require.js');
 
-    require.config({
-        baseUrl: './',
-        paths: {
-            jquery: 'lib/jquery',
-            underscore: 'lib/underscore'
-        },
-        shim: {
-            underscore: {
-                exports: '_'
-            }
+require.config({
+    baseUrl: './',
+    paths: {
+        jquery: 'lib/jquery',
+        underscore: 'lib/underscore'
+    },
+    shim: {
+        underscore: {
+            exports: '_'
         }
-    });
+    }
+});
 
-    require(['util/graph'], function(Graph) {
-        function runGraph() {
-            // TODO: generate graph from ev.data
-            return mainEvent.data;
-        }
+addEventListener('message', function(mainEvent) {
+    console.debug('hello');
+});
 
-        self.postMessage(runGraph());
-        var graph = new Graph.Graph();
-        var a = graph.addNode("a");
-        var b = graph.addNode("b");
-        var c = graph.addNode("c");
-        graph.addEdge(a, b);
-        graph.addEdge(a, c);
-        graph.addEdge(c, b);
-        graph.addEdge(c, a);
-        console.debug(graph.toDictionary());
-    });
-}
+require(['util/graph'], function(Graph) {
+    'use strict';
 
-// This code triggers the main code when it receives an event
-self.addEventListener('message', function(ev) {
-    messageHandler(ev);
+    var graph = new Graph.Graph();
+    var a = graph.addNode('a');
+    var b = graph.addNode('b');
+    var c = graph.addNode('c');
+    graph.addEdge(a, b);
+    graph.addEdge(a, c);
+    graph.addEdge(c, b);
+    graph.addEdge(c, a);
+    var dict = graph.toDictionary();
 });
